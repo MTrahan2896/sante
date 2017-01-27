0fe3ca70c8d52b8b0ab63d26b574b1c0cfb54b49
Jonathan Lafrenière <jonathan.lafreniere.02@edu.cegeptr.qc.ca>
2017-01-27 08:24:43 -0500

    Structure du dossier

---
+++ Pull.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+git pull
+echo.   
+echo.   
+echo.   
+echo                        _. - ~ ~ ~ - .
+echo     ..       __.    .-~               ~-.
+echo     ((\     /   `}.~                     `.
+echo      \\\   {     }               /     \   \         --------------------
+echo  (\   \\~~       }                     }   \         FIN DE LA SAUVEGARDE
+echo   \`.-~ -@~     }  ,-,.                )    \        --------------------
+echo   (___     ) _}  (    :            / /      `.
+echo    `----._-~.     _\ \ _       \   / /- _      `.
+echo           ~~----~~  \ \ ~~--~~~(  + /     ~-.     ~- _
+echo                     /  /         \  \          ~- . _ _~_-_.
+echo                  __/  /          _\  )
+echo                .k___.'         .k___/
+echo.   
+echo.   
+echo.                    ¯\_(ツ)_/¯
+pause
+exit
