facd2874afa0d7e8954102e3fd788ba043d7c096
Jonathan Lafrenière <jonathan.lafreniere.02@edu.cegeptr.qc.ca>
2017-01-30 17:42:15 -0500

    module connexion

--- developpement/Joe/js/scripts.js
+++ developpement/Joe/js/scripts.js
@@ -34,3 +34,14 @@ viewRender: function(view, element){
     }
 
     });
+    
+    $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .3, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+    
+    }
+    );
