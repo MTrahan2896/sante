5e5a268ba244836804aa1d1cab9bc13b46e94aac
Jonathan Lafrenière <jonathan.lafreniere.02@edu.cegeptr.qc.ca>
2017-01-30 17:34:42 -0500

    Template de l'application

--- developpement/Joe/js/scripts.js
+++ developpement/Joe/js/scripts.js
@@ -1,5 +1,5 @@
 let t= "test";
-
+angular.module('myModule', ['scDateTime']);
 $('.datepicker').pickadate({selectYears: 20});
 
 
@@ -11,5 +11,26 @@ $('.datepicker').pickadate({selectYears: 20});
         googleCalendarApiKey: 'AIzaSyCOz5Z77z9O4gDAvCmmC2ya1y-NUETMQ4w',
         events: {
             googleCalendarId: 'jonathan.lafreniere@gmail.com',
-        }
+        },
+     
+    });
+
+ var newEvent = new Object();
+
+newEvent.title = "some text\n";
+newEvent.start = new Date();
+newEvent.allDay = true;
+ var newEvent2 = new Object();
+
+newEvent2.title = "some tesx2\n";
+newEvent2.start = new Date();
+newEvent2.allDay = true;
+$('#calendar').fullCalendar( 'renderEvent', newEvent2 );
+
+$("#calendar").fullCalendar({
+
+viewRender: function(view, element){
+        alert(t);
+    }
+
     });
