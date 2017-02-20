a20273413b8b6bacd049da1c29e90d00c0b914a6
christophe bélanger <christophe blanger>
2017-02-20 12:21:28 -0500

    SYNC DE LACHE

--- developpement/xXxX_MasterPenguin02_XxXx aka Christ/accueil.php
+++ developpement/xXxX_MasterPenguin02_XxXx aka Christ/accueil.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <meta charset="UTF-8">
         <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
@@ -16,6 +17,9 @@
         }           
         .fc-day-grid-event > .fc-content {
              white-space: normal;
+        }
+        .modalinsact{
+            bottom:35% !important;
         }
       </style>
     </head>
@@ -46,11 +50,11 @@
       <option value="" disabled selected>Choisissez une activité</option>
       <option value="n">Tout les sports</option>
       <?php
-            $mysqli = new mysqli("localhost", "root", "", "defi_sante");
+            $mysqli = new mysqli("localhost", "root", "", "bd_application");
                     if ($mysqli->connect_errno) {
                         echo "Erreur de connection vers MYSQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
                     }
-                    $query = "SELECT distinct nom FROM activite";
+                    $query = "SELECT distinct nom FROM activites";
                     $result = $mysqli->query($query);
 
                     if ($result->num_rows > 0) {
@@ -70,6 +74,49 @@
 
 	</form>
 </div>
+<!-- Modal Structure -->
+  <div id="modalinsact" class="modal" >
+      <div class="container">
+          
+     
+    <div class="modal-content">
+        <form id="inscAct" method="POST" action="" hidden>
+              <input type="text" id="id_act" name="id_activite" value="">
+        </form>  
+        <div class="row">
+          <h3 id="titre" style="margin-bottom:0px;text-align:center;"></h3>
+        </div>
+        <div class="row">
+            <div class="col l4" style="margin-top:14px;text-align:right">
+                Date:
+            </div>
+            <div class="input-field col l8" style="margin-top:0px;">
+                <input type="date"  id="date"  readonly>
+            </div>  
+        </div>
+        <div class="row">
+            <div class="col l4" style="margin-top:14px;text-align:right">
+                Debut:
+            </div>
+            <div class="input-field col l8" style="margin-top:0px;">
+                <input type="time"  id="debut"  readonly>
+            </div>  
+        </div>
+        <div class="row">
+            <div class="col l4" style="margin-top:14px;text-align:right">
+                Fin:
+            </div>
+            <div class="input-field col l8" style="margin-top:0px;">
+                <input type="time"  id="fin"  readonly>
+            </div> 
+        </div>
+        <div class="row">
+            <button class="btn green col l12" type="submit"  form="inscAct">S'inscrire</button>
+        </div>
+    </div>
+    
+     </div>
+  </div>
 </main>
 
         
@@ -82,7 +129,7 @@
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
       <script src="js/moment.js">moment.locale="fr"</script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/locale/fr-ca.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/locale/fr-ca.js"></script>
      
       <script type="text/javascript" src="js/materialize.min.js"></script>
 		
@@ -90,52 +137,51 @@
          <script type="text/javascript" src="js/fullcalendar-fr.js"></script>
 		<script type="text/javascript" src="js/gcal.js"></script>
 		<script type="text/javascript" src="js/sc-date-time.js"></script>
-        <script>
+<script>
 
         //initialisation du tableau de base des evenements du calendrier
-        var view;
+    var view;
         eventbase = [
                     <?php
-                    $mysqli = new mysqli("localhost", "root", "", "defi_sante");
+                    $mysqli = new mysqli("localhost", "root", "", "bd_application");
                     if ($mysqli->connect_errno) {
                         echo "Erreur de connection vers MYSQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
                     }
-                    $query = "SELECT ap.id_activite_planif, a.couleur, a.nom, ap.date_activite, ap.debut, ap.fin FROM activite a, activite_planifie ap
-                              where a.id_activite = ap.id_activite";
+                    $query = "SELECT ap.ID_Activite_prevue, a.Couleur, a.Nom_Activite, ap.Date_Activite, ap.Heure, a.Duree FROM activites a, activites_prevues ap
+                              where a.ID_Activite = ap.ID_Activite";
                     $result = $mysqli->query($query);
 
                     if ($result->num_rows > 0) {
                         $i = 1;
     
                         while($row = $result->fetch_assoc()) {
-                            echo "{ title:'".$row['nom']."', start:'".$row['date_activite']."T".$row['debut']."', end:'".$row['date_activite']."T".$row['fin']."', allday: false,  id:".$row['id_activite_planif'].", backgroundColor:'#".$row['couleur']."', borderColor:'black'}";
+                            //strtotime("+15 minutes", strtotime($row['heure']))
+                            echo "{ title:'".$row['Nom_Activite']."', start:'".$row['Date_Activite']."T".$row['Heure']."', end:'".$row['Date_Activite']."T".$row['Heure']."', allday: false,  id:".$row['ID_Activite_prevue'].", backgroundColor:'#".$row['Couleur']."', borderColor:'black'}";
                             if ($result->num_rows > $i) {
                                 echo ",";
                             }
                         }
                     }
-                    ?>];
+                    ?>
+                    ];
                     evenements = eventbase;
-        function TrieCalendrier(s){
-            view = $('#calendar').fullCalendar('getView');
-            event_trie = [];
-            if(s != "n"){
-                for(i = 0;i<eventbase.length;i++){
-                    if(eventbase[i]['title'] == s){
-                        event_trie.push(eventbase[i]);
-                    }
+    function TrieCalendrier(s){
+        view = $('#calendar').fullCalendar('getView');
+        event_trie = [];
+        if(s != "n"){
+            for(i = 0;i<eventbase.length;i++){
+                if(eventbase[i]['title'] == s){
+                    event_trie.push(eventbase[i]);
                 }
-                evenements = event_trie;
-            }else if(s == "n"){
-                evenements=eventbase;
             }
+            evenements = event_trie;
+        }else if(s == "n"){
+             evenements=eventbase;
+        }
             
-            
-           
-
-            //appel de l'initialisation du calendrier
-            $('#calendar').fullCalendar('destroy');
-            inicalendrier();
+        //appel de l'initialisation du calendrier
+        $('#calendar').fullCalendar('destroy');
+        inicalendrier();
         }
 
         //initialisation du calendrier
@@ -143,16 +189,34 @@
             
             $('#calendar').fullCalendar({
                 header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-            slotEventOverlap: false,
-            allDaySlot: false,
+				    left: 'prev,next today',
+				    center: 'title',
+				    right: 'month,agendaWeek,agendaDay'
+			    },
+                slotEventOverlap: false,
+                allDaySlot: false,
                 events: evenements,
                 eventClick: function(event) {
-                        alert(event.id);
-                    }
+                    
+                    $('#modalinsact').modal('open');
+                    $('#id_act').attr({
+                        value: event.id
+                    });
+                    $('#titre').text(event.title);
+                    
+                    $('#date').attr({
+                        value:event.start.toISOString().substring(0,event.start.toISOString().indexOf("T")).replace(new RegExp('/', 'g'), '-')
+                    });
+                    //$('#debut').text("Debut: "+ event.start.toISOString().substring(event.start.toISOString().indexOf("T")+1,16));
+                    $('#debut').attr({
+                        value:event.start.toISOString().substring(event.start.toISOString().indexOf("T")+1,16)
+                    });
+                    //$('#fin').text("Fin:"+event.end.toISOString().substring(event.end.toISOString().indexOf("T")+1,16));
+                    $('#fin').attr({
+                        value:event.end.toISOString().substring(event.end.toISOString().indexOf("T")+1,16)
+                    });
+
+                }
             })
             
             $('#calendar').fullCalendar( 'changeView', view.name )
--- developpement/xXxX_MasterPenguin02_XxXx aka Christ/components/nav.php
+++ developpement/xXxX_MasterPenguin02_XxXx aka Christ/components/nav.php
@@ -1,29 +1,45 @@
-
  <nav>
+
+
       <div class="nav-wrapper">
         <a href="#" class="brand-logo center">Défi Santé</a>
         <ul id="nav-mobile" class="left hide-on-med-and-down">
           <li><a href="accueil.php">Accueil</a></li>
-          <li><a href="sass.html">Administration</a></li>
+          <li><a class="adminTabs" href="administration.php">Administration</a></li>
           <li><a href="collapsible.html"></a></li>
+          
         </ul>
         
         <?php
-       SESSION_START();
+        if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+        if(!isset($_SESSION['uid'])){
+         $_SESSION['uid'] = 0;
+          echo "<script> document.getElementById('Greet_User').innerHTML ='".$_SESSION['username']."'; $('.adminTabs').remove();</script>";
+       
+        }
         if ($_SESSION['uid'] == 0)
         {
         echo '
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <ul id="nav-mobile" class="right">
+        
         <li><a data-target="modal_login"><i class="material-icons prefix">account_circle</i></a></li>
         </ul>';
+
         }
         else
         {     
           echo '
             <form action="Accueil.php" method="POST">
-              <button class="btn waves-effect waves-light right" style="margin-top:14px;margin-right:10px;" type="submit" name="deconnexion">Déconnexion
-              </button>
+            <ul id="nav-mobile" class="right">
+            <li style="margin-right: 20px"><span id="Greet_User"></span></li>
+            <li>  <button class="btn blue waves-effect waves-light right" style="margin-top:14px;margin-right:10px;" type="submit" name="deconnexion">Déconnexion
+              </button></li>
             </form>';
+
+           echo "<script> document.getElementById('Greet_User').innerHTML ='Bonjour ".$_SESSION['username']."'</script>";
 
           if (isset($_POST['deconnexion']))
           {
@@ -31,11 +47,17 @@
             header('Location: accueil.php');
           }
         }
+        
+
+
+
         ?>
 
       </div>
     </nav>
 
     <?php
-    include 'connexion.php';
+    include 'modal_connexion.php';
     ?>
+
+
